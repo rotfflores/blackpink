5d4749bb882fe53accd91adde99381c64087e0ea
rotfflores <cli0001091@alumnos.ujed.mx>
2024-04-06 12:03:06 -0600

    video responsive

--- js/rose.js
+++ js/rose.js
@@ -4,8 +4,22 @@ document.addEventListener("DOMContentLoaded", function() {
       // Establecer el tiempo de inicio en el segundo 30 después de que se carguen los metadatos
       video.currentTime = 15;
     });
-  
-    video.oncanplay = function() {
-      video.play();
-    };
-  });
+  }
+  );
+
+  document.addEventListener("DOMContentLoaded", function() {
+    var video = document.getElementById("video-mobile");
+
+    // Detectar si el dispositivo es móvil
+    var isMobile = /iPhone|iPad|iPod|Android/i.test(navigator.userAgent);
+    if (isMobile) {
+        video.setAttribute('autoplay', '');
+        video.setAttribute('muted', '');
+        video.setAttribute('playsinline', '');
+    }
+
+    video.addEventListener("loadedmetadata", function() {
+        // Establecer el tiempo de inicio en el segundo 15 después de que se carguen los metadatos
+        video.currentTime = 15;
+    });
+});
