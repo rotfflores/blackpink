9424b484ee69cdb36dff3533b19212a79ca4f19a
rotfflores <cli0001091@alumnos.ujed.mx>
2024-04-16 22:20:38 -0600

    no se que poner

--- js/home.js
+++ js/home.js
@@ -3,3 +3,9 @@ const rose = document.getElementById('rose');
 rose.addEventListener('click', function() {
     window.location.href = 'rose.html';
 });
+
+const jennie = document.getElementById('jennie');
+
+jennie.addEventListener('click', function() {
+    window.location.href = 'jennie.html';
+});
